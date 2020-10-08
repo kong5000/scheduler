3a380f18e01924a3a4e8e7ab2202d5e980c45afa
kong5000 <keith.ong5000@gmail.com>
2020-10-08 09:21:33 -0700

    Fix bug were websocket does not update spots remainin

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -58,7 +58,7 @@ export default function () {
     })
   }
 
-  function websocketBookInterview(id, interview) {
+  function updateInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -72,7 +72,7 @@ export default function () {
     setState({ ...state, appointments })
   }
 
-  function websocketCancelInterview(id) {
+  function updateCancelInterview(id) {
     const appointment = {
       ...state.appointments[id],
       interview: null
@@ -86,7 +86,9 @@ export default function () {
     const days = [
       ...state.days
     ]
-    setState({ ...state, appointments, days })
+
+
+      setState({ ...state, appointments, days })
   }
 
 
@@ -125,19 +127,22 @@ export default function () {
     const type = message.type;
     if (type === "SET_INTERVIEW") {
       if(message.interview === null){
-        websocketCancelInterview(message.id)
+        updateCancelInterview(message.id)
       }else{
-        websocketBookInterview(message.id, message.interview)
+        updateInterview(message.id, message.interview)
       }
+      Promise.all([
+        axios.get('http://localhost:8001/api/days'),
+        axios.get('http://localhost:8001/api/appointments'),
+        axios.get('http://localhost:8001/api/interviewers')
+      ]).then((all) => {
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      })
     }
   };
 
 
   useEffect(() => {
-
-
-
-
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
       axios.get('http://localhost:8001/api/appointments'),
