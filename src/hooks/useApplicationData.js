06977e7237e3d7720aaa02920406d67b3d14cb36
kong5000 <keith.ong5000@gmail.com>
2020-10-08 09:34:28 -0700

    Made updateStateFromServer function to reduce duplication

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { func } from 'prop-types';
 
 
 
@@ -121,6 +122,16 @@ export default function () {
     })
   }
 
+  function updateStateFromServer(){
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    })
+  }
+
   webSocket.onmessage = event => {
     console.log(`Message Received: ${event.data}`);
     const message = JSON.parse(event.data)
@@ -131,25 +142,13 @@ export default function () {
       }else{
         websocketUpdateInterview(message.id, message.interview)
       }
-      Promise.all([
-        axios.get('http://localhost:8001/api/days'),
-        axios.get('http://localhost:8001/api/appointments'),
-        axios.get('http://localhost:8001/api/interviewers')
-      ]).then((all) => {
-        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-      })
     }
+    updateStateFromServer();
   };
 
 
   useEffect(() => {
-    Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
-    ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    })
+    updateStateFromServer();
   }, [])
 
   return {
